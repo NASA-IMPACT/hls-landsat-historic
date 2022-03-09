7f19be82b96865cf59bca19ee0aa8af48db6dd35
Matthew Hicks <mh0051@uah.edu>
2022-03-09 11:04:10 -0600

    Feature add: Historic dl lambda

--- cdk/stack.py
+++ cdk/stack.py
@@ -15,6 +15,8 @@ from aws_cdk import (
 LAST_DATE_INITIAL = os.getenv("LANDSAT_HISTORIC_LAST_DATE_INITIAL")
 DAYS_RANGE = os.getenv("LANDSAT_HISTORIC_DAYS_RANGE")
 CRON_STRING = os.getenv("LANDSAT_HISTORIC_CRON_STRING")
+USGS_S3_LANDSAT_INVENTORY_BUCKET = os.getenv("LANDSAT_HISTORIC_INVENTORY_BUCKET")
+INV_DL_CRON_STRING = os.getenv("LANDSAT_HISTORIC_INVENTORY_DL_CRON_STRING")
 
 if os.getenv("LANDSAT_HISTORIC_GCC", None) == "true":
     GCC = True
@@ -88,6 +90,33 @@ class LandsatHistoricStack(core.Stack):
 
         self.rule.add_target(
             aws_events_targets.LambdaFunction(self.subset_granules_function)
+        )
+
+        self.historic_dl_rule = aws_events.Rule(
+            self,
+            f"{stack_name}_inventory_dl_cron_rule",
+            schedule=aws_events.Schedule.expression(INV_DL_CRON_STRING),
+        )
+
+        self.download_historic_usgs_csv = aws_lambda_python.PythonFunction(
+            self,
+            id=f"{stack_name}-download_historic_usgs_csv",
+            entry="lambdas",
+            handler="handler",
+            index="download_historic_landsat_csv.py",
+            runtime=aws_lambda.Runtime.PYTHON_3_8,
+            memory_size=5000,
+            timeout=core.Duration.minutes(15),
+            role=self.role,
+            environment={
+                "BUCKET": self.landsat_inventory_bucket.bucket_name,
+                "KEY": "inventory_product_list.zip",
+                "COPY_BUCKET": USGS_S3_LANDSAT_INVENTORY_BUCKET,
+            },
+        )
+
+        self.historic_dl_rule.add.add_target(
+            aws_events_targets.LambdaFunction(self.download_historic_usgs_csv)
         )
 
         self.landsat_inventory_bucket.grant_read(self.role)
